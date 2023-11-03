b4623cf810525806113fafe356e71cb95d943fa5
hqsh37 <hqsh37@gmail.com>
2023-11-03 22:38:24 +0700

    initial code

---
+++ header.php
@@ -0,0 +1,53 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light ct-header">
+        <a class="navbar-brand" href="#">
+            <img class="logo" width="150px" height="80px" src="./assets/img/logo.png" alt="logo">
+        </a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">Trang Chủ</button>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">Tài Khoản</button>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">
+                            QL Thu Chi
+                        </button>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">Báo Cáo</button>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">Cảnh Báo Chi Tiêu</button>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn btn-outline-secondary ct-font">Chức Năng Khác</button>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="user">
+            <div class="ct-name">
+                <p>Hoang Quang Sang</p>
+            </div>
+            <div class="avt">
+                <img src="./assets/img/noimg.jpg" alt="logo">
+            </div>
+        </div>
+
+    </nav>
+</header>
